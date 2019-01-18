33996c86351af694d07a573924573fcbe53448d7
tonyallain <tonyallain11144@hotmail.com>
2019-01-17 16:22:17 -0800

    setting up redux

---
+++ src/reducers/app-reducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+    isRunning: true
+};
+
+const appReducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+export default appReducer;
